0d75143985d92b236aefc6ba39713154ed4d1661
DESKTOP-14NC32I\Maya Lemmon-Kishi <mel118@pitt.edu>
2018-01-28 02:38:49 -0500

    Please don't break

---
+++ Stuff/Attempt2/Assets/Controller2.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Controller2:
+    MonoBehaviour
+{
+
+    public Player player;
+    public GameObject enemyPrefab;
+    public float enemySpawnDistance = 25f;
+
+    public float enemyInterval = 4.0f;
+    public float minimumEnemyInterval = 2f;
+    public float enemyIntervalDecrement = 0.1f;
+
+    private float gameTimer = 0f;
+    private float enemyTimer = 0f;
+
+    public TextMesh infoText;
+
+    private float resetTimer = 3f;
+    private float gameTime = 15f;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.health > 0 && gameTimer <= gameTime)
+        {
+            //infoText.text = "Health: " + player.health;
+            infoText.text = "\nPoints: " + player.points;
+            gameTimer += Time.deltaTime;
+            //infoText.text += "\nTime: " + Mathf.Floor(gameTimer);
+        }
+        else
+        {
+            if (player.health <= 0)
+            {
+                infoText.text = "Game Over!";
+                infoText.text += "\nYou survived for " + Mathf.Floor(gameTimer) + " seconds!";
+                resetTimer -= Time.deltaTime;
+
+                SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+            }
+            else
+            {
+                infoText.text = "You Won!!";
+                infoText.text = "You earned " + player.points + "points!";
+                SceneManager.LoadScene("HiddenFiguresEnding");
+            }
+        }
+
+
+        enemyTimer -= Time.deltaTime;
+        if (enemyTimer <= 0)
+        {
+            enemyTimer = enemyInterval;
+            enemyInterval -= enemyIntervalDecrement;
+
+
+            if (enemyInterval < minimumEnemyInterval)
+            {
+                enemyInterval = minimumEnemyInterval;
+            }
+
+            GameObject enemyObject = Instantiate(enemyPrefab);
+
+            Enemy enemy = enemyObject.GetComponent<Enemy>();
+
+            float randomAngle = Random.Range(0f, Mathf.PI * 2f);
+            enemy.transform.position = new Vector3(
+                player.transform.position.x + Mathf.Cos(randomAngle) * enemySpawnDistance,
+                enemy.transform.position.y,
+                player.transform.position.z + Mathf.Sin(randomAngle) * enemySpawnDistance
+             );
+
+            enemy.player = player;
+            enemy.direction = (player.transform.position - enemy.transform.position).normalized;
+            enemy.transform.LookAt(player.transform);
+        }
+
+    }
+}
